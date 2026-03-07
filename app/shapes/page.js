c192744ea26f9167a2b4430438c8481deee5993c
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2026-03-07 13:01:11 +0600

    updated code

--- app/shapes/page.js
+++ app/shapes/page.js
@@ -1,5 +1,7 @@
+import { Suspense } from "react";
 import DxfEditor from "./DxfEditor";
 import { getLanguage } from "../lib/i18n/getLanguage";
+import { Spin } from "antd";
 
 export default async function page() {
   const { lang, dict } = await getLanguage();
@@ -7,7 +9,9 @@ export default async function page() {
 
   return (
     <div>
-      <DxfEditor lang={lang} shapesText={shapesText} />
+      <Suspense fallback={<Spin size="large" />}>
+        <DxfEditor lang={lang} shapesText={shapesText} />
+      </Suspense>
     </div>
   );
 }
